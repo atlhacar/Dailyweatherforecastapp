3f685822f481411a75d407450a68ac0ed3f53de8
atlhacar <ahmttlhacar01@gmail.com>
2022-07-20 17:06:14 +0300

    weatherapp

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '242ffd79bbmshfc5f1ee5bce5344p1146ccjsn3d96d74bba7b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "304b967a59583a6fb428c65bc2233697";
+
